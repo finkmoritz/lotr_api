cf45f455a108be28d202a6267c430abc43ff9cf2
Moritz Fink <moritz.fink@accenture.com>
2022-02-01 14:53:52 +0100

    Refactoring

--- lib/example/movie_example.dart
+++ lib/example/movie_example.dart
@@ -2,7 +2,7 @@ import 'package:the_one_api/the_one_api.dart';
 
 void main(List<String> args) async {
   if (args.isEmpty) {
-    throw Exception('Expected API key as last argument, but received none.');
+    throw Exception('Expected API key as first argument, but received none.');
   }
 
   var theOneApi = TheOneApi(
