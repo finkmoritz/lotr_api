0d1964719c7f7e8578e22fc2cbf302e272c9a374
Moritz Fink <moritz.fink@accenture.com>
2022-02-02 13:24:27 +0100

    Rework endpoints with IdFilter

--- lib/src/the_one_api.dart
+++ lib/src/the_one_api.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:the_one_api/src/query/filter/chapter_name_filter.dart';
 import 'package:the_one_api/src/query/filter/id_filter.dart';
+import 'package:the_one_api/src/query/filter/name_filter.dart';
 
 import 'config/api_version.dart';
 import 'model/book.dart';
@@ -29,6 +31,7 @@ class TheOneApi {
   Future<Response<Book>> getBooks({
     Pagination? pagination,
     IdFilter? idFilter,
+    NameFilter? nameFilter,
   }) async {
     return _getResponse<Book>(
       mapping: (b) => Book.fromJson(b),
@@ -36,6 +39,7 @@ class TheOneApi {
       pagination: pagination,
       filters: [
         idFilter,
+        nameFilter,
       ],
     );
   }
@@ -51,25 +55,31 @@ class TheOneApi {
 
   Future<Response<Chapter>> getBookChapters({
     required String bookId,
-    String? chapterId,
+    Pagination? pagination,
+    IdFilter? idFilter,
+    ChapterNameFilter? chapterNameFilter,
   }) async {
     return _getResponse<Chapter>(
       mapping: (c) => Chapter.fromJson(c),
       endpoint: 'book/${bookId}/chapter',
-      queries: [
-        '_id=${chapterId ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
+        chapterNameFilter,
       ],
     );
   }
 
   Future<Response<Movie>> getMovies({
-    String? id,
+    Pagination? pagination,
+    IdFilter? idFilter,
   }) async {
     return _getResponse<Movie>(
       mapping: (b) => Movie.fromJson(b),
       endpoint: 'movie',
-      queries: [
-        '_id=${id ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
       ],
     );
   }
@@ -78,32 +88,36 @@ class TheOneApi {
     required String id,
   }) async {
     Response<Movie> movies = await getMovies(
-      id: id,
+      idFilter: IdFilter()..matches = id,
     );
     return movies.docs.isNotEmpty ? movies.docs.first : null;
   }
 
   Future<Response<Quote>> getMovieQuotes({
     required String movieId,
-    String? quoteId,
+    Pagination? pagination,
+    IdFilter? idFilter,
   }) async {
     return _getResponse<Quote>(
       mapping: (c) => Quote.fromJson(c),
       endpoint: 'movie/${movieId}/quote',
-      queries: [
-        '_id=${quoteId ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
       ],
     );
   }
 
   Future<Response<Character>> getCharacters({
-    String? id,
+    Pagination? pagination,
+    IdFilter? idFilter,
   }) async {
     return _getResponse<Character>(
       mapping: (b) => Character.fromJson(b),
       endpoint: 'character',
-      queries: [
-        '_id=${id ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
       ],
     );
   }
@@ -112,70 +126,70 @@ class TheOneApi {
     required String id,
   }) async {
     Response<Character> characters = await getCharacters(
-      id: id,
+      idFilter: IdFilter()..matches = id,
     );
     return characters.docs.isNotEmpty ? characters.docs.first : null;
   }
 
   Future<Response<Quote>> getCharacterQuotes({
     required String characterId,
-    String? quoteId,
+    Pagination? pagination,
+    IdFilter? idFilter,
   }) async {
     return _getResponse<Quote>(
       mapping: (c) => Quote.fromJson(c),
       endpoint: 'character/${characterId}/quote',
-      queries: [
-        '_id=${quoteId ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
       ],
     );
   }
 
   Future<Response<Quote>> getQuotes({
-    String? id,
+    Pagination? pagination,
+    IdFilter? idFilter,
   }) async {
     return _getResponse<Quote>(
       mapping: (c) => Quote.fromJson(c),
       endpoint: 'quote',
-      queries: [
-        '_id=${id ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
       ],
     );
   }
 
   Future<Quote?> getQuote({
-    String? id,
+    required String id,
   }) async {
-    Response<Quote> quotes = await _getResponse<Quote>(
-      mapping: (c) => Quote.fromJson(c),
-      endpoint: 'quote',
-      queries: [
-        '_id=${id ?? ''}',
-      ],
+    Response<Quote> quotes = await getQuotes(
+      idFilter: IdFilter()..matches = id,
     );
     return quotes.docs.isNotEmpty ? quotes.docs.first : null;
   }
 
   Future<Response<Chapter>> getChapters({
-    String? id,
+    Pagination? pagination,
+    IdFilter? idFilter,
+    ChapterNameFilter? chapterNameFilter,
   }) async {
     return _getResponse<Chapter>(
       mapping: (c) => Chapter.fromJson(c),
       endpoint: 'chapter',
-      queries: [
-        '_id=${id ?? ''}',
+      pagination: pagination,
+      filters: [
+        idFilter,
+        chapterNameFilter,
       ],
     );
   }
 
   Future<Chapter?> getChapter({
-    String? id,
+    required String id,
   }) async {
-    Response<Chapter> chapters = await _getResponse<Chapter>(
-      mapping: (c) => Chapter.fromJson(c),
-      endpoint: 'chapter',
-      queries: [
-        '_id=${id ?? ''}',
-      ],
+    Response<Chapter> chapters = await getChapters(
+      idFilter: IdFilter()..matches = id,
     );
     return chapters.docs.isNotEmpty ? chapters.docs.first : null;
   }
@@ -183,7 +197,6 @@ class TheOneApi {
   Future<Response<T>> _getResponse<T>({
     required T Function(dynamic) mapping,
     required String endpoint,
-    List<String?>? queries,
     Pagination? pagination,
     List<Filter?> filters = const [],
   }) async {
@@ -194,7 +207,6 @@ class TheOneApi {
 
     String url = '${_baseUrl}/${endpoint}?';
 
-    queries?.forEach((query) => url += '${query}&');
     pagination?.getQueries().forEach((query) => url += '${query}&');
     filters.forEach((filter) {
       filter?.getQueries().forEach((query) => url += '${query}&');
